223d4826782290e50097e349eb20a328499e8501
Kyle Ochate <kyleochate@gmail.com>
2022-04-01 13:48:49 -0400

    Final Push

--- src/main/java/labTwo/Main.java
+++ src/main/java/labTwo/Main.java
@@ -1,46 +1,26 @@
 package labTwo;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     //initial setup
     private String username;
     private String defaultPass;
-    private String invalidMessage;
     private static boolean validPass;
-//    public char[] specialchars = {'!','@','#','$','%','^','&','*','(',')','_','+','-','=',',','.','/',';','"',':','<','>','?','[','{',']','}'};
 
     //getter and setter for username
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    //getter and setter for password
-    public String getDefaultPass() {
-        return defaultPass;
-    }
-    public void setDefaultPass(String defaultPass) {
-        this.defaultPass = defaultPass;
-    }
-    public boolean validPass(){
-        return validPass;
-    }
-    public void closeUserinput(){
-        userInput.close();
-    }
+
+
     public static Scanner userInput = new Scanner(System.in);
-    public Main(String username, String defaultPass){
-        this.username=username;
-        this.defaultPass=defaultPass;
+
+    public Main(String username, String defaultPass) {
+        this.username = username;
+        this.defaultPass = defaultPass;
     }
 
     public static void main(String[] args) {
-//        char[] specialchars = {'!','@','#','$','%','^','&','*','(',')','_','+','-','=',',','.','/',';','"',':','<','>','?','[','{',']','}'};
-//        int upperCount = 0;
-//        int specialCount = 0;
+        char[] specialchars = {'!','@','#','$','%','^','&','*','(',')','_','+','-','=',',','.','/',';','"',':','<','>','?','[','{',']','}'};
+        boolean isValid = true;
 
         System.out.println("Please enter your username below: ");
         String username = userInput.nextLine();
@@ -55,28 +35,59 @@ public class Main {
         System.out.println("-not contain your username.");
         System.out.println("-be different from your old password.");
 
-        while (true){
+        while (isValid==true) {
+            boolean isUpper = false;
+            boolean isSpecial = false;
+            boolean isLong = false;
+            boolean isNotuser = false;
+            boolean isDifferent = false;
+
             System.out.println("Please enter your new password below: ");
             String newPass = userInput.nextLine();
 
-//            for (int i = 0; i++){
-//                char check = userInput.charAt(i);
 
             if (newPass.length() < 8) {
                 validPass = false;
                 System.out.println("Your new password must be at least 8 characters");
-            }else if (newPass.toUpperCase().contains(username.toUpperCase())){
+            }if (newPass.length() >= 8) {
+                isLong = true;
+
+            }if (newPass.toUpperCase().contains(username.toUpperCase())) {
                 validPass = false;
                 System.out.println("Your new password must not contain your username!");
-            }else if (newPass.equals(password)){
+            }if (!newPass.toUpperCase().contains(username.toUpperCase())) {
+                isNotuser = true;
+
+            }if (newPass.equals(password)) {
                 validPass = false;
                 System.out.println("Your new password must be different from your current password!");
+            }if (!newPass.equals(password)){
+                isDifferent = true;
+            }for (int i = 0; i < newPass.length(); i++) {
+                if (Character.isUpperCase(newPass.charAt(i))) {
+                    isUpper = true;
+                }
+            }if (isUpper == false){
+                System.out.println("Your new password must contain an uppercase letter.");
+            }for (int x=0; x < newPass.length(); x++){
+                for (int y=0; y< specialchars.length; y++){
+                    if (newPass.charAt(x)==specialchars[y]){
+                        isSpecial=true;
+                    }
+                }
+            }if (isSpecial==false){
+                System.out.println("Your new password must contain a special character.");
             }
-
-
-
+            if (isLong&&isNotuser&&isDifferent&&isUpper&&isSpecial){
+                isValid=false;
+//                System.out.println("Test");
+            }
+//            System.out.println("isDifferent: " + isDifferent);
+//            System.out.println("isLong: " +isLong);
+//            System.out.println("isNotUser: " +isNotuser);
+//            System.out.println("isSpecial: " +isSpecial);
+//            System.out.println("isUpper: " + isUpper);
+                }
+        System.out.println("Password is Valid");
+            }
         }
-
-
-    }
-}
