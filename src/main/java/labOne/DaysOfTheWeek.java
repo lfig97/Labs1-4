16808cdac0c03426f369fc361d0ac76c82d1d93c
Luis Figueroa <lfigueroa002@my.wilmu.edu>
2022-03-31 21:10:52 -0400

    Adding class

---
+++ src/main/java/labOne/DaysOfTheWeek.java
@@ -0,0 +1,20 @@
+package labOne;
+
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+    private int dayOfWeek;
+
+    String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+            "Saturday", "Sunday"};
+
+    public void askNumber(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a number that corresponds to a day in the week:");
+        this.dayOfWeek = myScanner.nextInt();
+    }
+
+    public void printDay(){
+        System.out.println(daysOfWeek[this.dayOfWeek-1]);
+    }
+}
--- src/main/java/labOne/Main.java
+++ src/main/java/labOne/Main.java
@@ -4,21 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter a number that corresponds to a day in the week:");
-        int dayOfWeek = myScanner.nextInt();
-
-
-        System.out.println(daysOfWeek[dayOfWeek -1 ]);
-
-
-
-
-
+        DaysOfTheWeek dow = new DaysOfTheWeek();
+        dow.askNumber();
+        dow.printDay();
 
     }
 }
